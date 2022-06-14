38f9036efbb33eb46b5544705501e9e0adeaa805
Samapuma <79788895+SamanthaWathenOlson@users.noreply.github.com>
2022-06-14 16:01:20 -0400

    Creating entities for project

---
+++ Data Access/Entities/Customer.py
@@ -0,0 +1,7 @@
+class Customer:
+
+    def __init__(self, maid_id, customer_name, customer_id):
+        self.maid_id = maid_id
+        self.customer_name = customer_name
+        self.customer_id = customer_id
+
---
+++ Data Access/Entities/Maid.py
@@ -0,0 +1,6 @@
+class Maid:
+
+    def __init__(self, maid_id, maid_name, customer_id):
+        self.maid_id = maid_id
+        self.maid_name = maid_name
+        self.customer_id = customer_id
