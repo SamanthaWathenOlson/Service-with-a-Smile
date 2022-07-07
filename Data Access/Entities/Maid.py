5ac6e6a5eccc49747a94a0b512b79d02cca4dc15
Samantha WathenOlson <samantha.wathen269@gmail.com>
2022-07-07 00:35:51 -0400

    Update Maid.py
    Creating function for maids

--- Data Access/Entities/Maid.py
+++ Data Access/Entities/Maid.py
@@ -1,10 +1,19 @@
 class Maid:
 
-    def __init__(self, maid_id, maid_name, customer_id):
+    def __init__(self, maid_id, maid_name, customer_id, read_message, send_message):
         self.maid_id = maid_id
         self.maid_name = maid_name
         self.customer_id = customer_id
+        self.read_message = read_message
+        self.send_message = send_message
 
-    def my funct(self):
-        print("Hello, master. My name is" + maid.name)
+    def myfunct(self):
+        print("Hello, master. My name is" + maid_name)
+
+    def readletter(self):
+        print("Your message says" + read_message)
+
+    def sendletter(self):
+        print("Your message says" + send_message)
+
 
